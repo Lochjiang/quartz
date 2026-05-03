95d22f29af77e4185bb4fa683b8a23e7f8729ed0
Lochjiang <1726469676@qq.com>
2026-05-03 13:18:44 +0800

    Quartz sync: May 3, 2026, 1:18 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "zh-CN",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian","附件文件夹"],
+    ignorePatterns: ["private", "templates", ".obsidian","测试隐藏文件夹"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
