2c34803f5794191ea0ffaa054e755d2ef6fc912b
Lochjiang <1726469676@qq.com>
2026-05-04 10:20:35 +0800

    Quartz sync: May 4, 2026, 10:20 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,19 +27,16 @@ export const sharedPageComponents: SharedLayout = {
     provider: 'giscus',
     options: {
             // from data-repo
-      repo: 'jackyzha0/quartz',
+      repo: 'Lochjiang/Lochjiang.github.io',
       // from data-repo-id
-      repoId: 'MDEwOlJlcG9zaXRvcnkzODcyMTMyMDg',
+      repoId: 'R_kgDOSRsvyg',
       // from data-category
       category: 'Announcements',
       // from data-category-id
-      categoryId: 'DIC_kwDOFxRnmM4B-Xg6',
+      categoryId: 'DIC_kwDOSRsvys4C8R9g',
       // from data-lang
-      lang: 'en',
+      lang: 'zh-CN',
       // Other options
-      themeUrl: "https://example.com/static/giscus", // corresponds to quartz/static/giscus/
-      lightTheme: "light-theme", // corresponds to light-theme.css in quartz/static/giscus/
-      darkTheme: "dark-theme", // corresponds to dark-theme.css quartz/static/giscus/
     }
   }),
   ],
