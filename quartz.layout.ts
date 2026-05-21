7919d5dc0273cfa62637745d950abe313aebc74b
Lochjiang <1726469676@qq.com>
2026-05-21 19:42:07 +0800

    Quartz sync: May 21, 2026, 7:42 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -93,7 +93,7 @@ export const defaultContentPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
-        // { Component: Component.ReaderMode() },
+        { Component: Component.ReaderMode() },
       ],
     }),
     Component.Explorer({
@@ -125,6 +125,7 @@ export const defaultListPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
       ],
     }),
     Component.Explorer({
