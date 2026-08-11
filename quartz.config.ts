a412a1bd0b66c52e92fe1caacd58287324776c3f
Lochjiang <1726469676@qq.com>
2026-08-11 11:30:14 +0800

    Quartz sync: Aug 11, 2026, 11:30 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "lochjiang.github.io/quartz",
+    baseUrl: "lochjiang.github.io/quartz/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
