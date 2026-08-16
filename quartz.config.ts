7375db6f6123f1590289b56d46d07bc1fb6b31ae
Lochjiang <1726469676@qq.com>
2026-08-16 17:19:36 +0800

    Quartz sync: Aug 16, 2026, 5:19 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        body: "Google Sans",
         // body: "LXGW WenKai TC",
         code: "IBM Plex Mono",
       },
